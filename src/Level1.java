d1c3bd07184ce3f5ccba5a21e681742e47771511
DavidCydeo <david@cydeo.com>
2024-02-29 20:15:33 +0300

    Level1 is created

---
+++ src/Level1.java
@@ -0,0 +1,15 @@
+public class Level1 {
+
+    // task1
+
+    //task2
+
+    //task3
+
+    public static void main(String[] args) {
+
+        System.out.println("Level1 of the game is ready");
+
+
+    }
+}
